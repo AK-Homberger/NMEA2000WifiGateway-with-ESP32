e4b72efd8910afe9cbe2ee85adcccf215d864f3f
AK-Homberger <53138495+AK-Homberger@users.noreply.github.com>
2021-02-24 22:40:30 +0100

    Update Wifi-Serial-AP-AIS-CAN.ino

--- Wifi-Serial-AP-AIS-CAN/Wifi-Serial-AP-AIS-CAN.ino
+++ Wifi-Serial-AP-AIS-CAN/Wifi-Serial-AP-AIS-CAN.ino
@@ -531,6 +531,7 @@ void loop() {
 
   int SourceAddress = NMEA2000.GetN2kSource();
   if (SourceAddress != NodeAddress) { // Save potentially changed Source Address to NVS memory
+    NodeAddress = SourceAddress;      // Set new Node Address (to save only once)
     preferences.begin("nvs", false);
     preferences.putInt("LastNodeAddress", SourceAddress);
     preferences.end();
